2d780771dcab460115a3af1ca87bd1350fbc9d65
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-28 16:39:01 +0100

    Esercizio "Saluta interlocutore" - Completato

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 <!-- PHP -->
 <?php
+  $name = $argv[1];
+  $surname = $argv[2];
+  echo "Ciao " . $name . " " . $surname;
 
-
-
+  $name = "Luigi";
 ?>
 <!-- /PHP -->
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="./dist/css/app.css">
+  </head>
+  <body>
+    <h1>Ciao <?php echo $name;?></h1>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Badwords</title>
-  <!-- CSS -->
-  <link rel="stylesheet" href="./dist/css/app.css">
-</head>
-
-<body>
-
-
-  <!-- VueJS -->
-  <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-  <!-- JS -->
-  <script src="./dist/js/app.js"></script>
-</body>
-
+    <!-- VueJS -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <!-- JS -->
+    <script src="./dist/js/app.js"></script>
+  </body>
 </html>
