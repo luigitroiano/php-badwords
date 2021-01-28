300500dd4a0ce05ce0cc8c37362c5ef8b8ade001
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-28 20:16:56 +0100

    Aggiunto Form
    Aggiunto input submit per inserire parola da censurare

--- index.php
+++ index.php
@@ -10,7 +10,8 @@ $name = "Luigi";
 Creare una variabile con un paragrafo di testo.
 Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la badword passata in GET con tre *. */
 $paragraph = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut obcaecati alias odio rerum quia voluptatum repudiandae commodi eos laborum inventore, iste provident ipsam dolore maxime, consequatur natus dicta culpa officia.";
-$censored = $_GET["badword"];
+$censored = $_POST["badword"];
+# $censored = $_GET["badword"]; da utilizzare se voglio mandare una richiesta "GET" col form
 $censored_word = str_replace($censored, "***", $paragraph);
 
 ?>
@@ -33,6 +34,19 @@ $censored_word = str_replace($censored, "***", $paragraph);
   <p>Questo paragrafo ha: <?php echo strlen($paragraph); ?> caratteri.</p>
   <h2>Paragrafo censurato:</h2>
   <p><?php echo $censored_word; ?></p>
+
+  <!-- Form creato con richiesta POST, a differenza di GET ci restituisce DATI direttamente nel "body" della richiesta. Ovviamente bisogna specificare l'attributo ACTION del form con il nome del file php perchè sennò non sa dove spedire i dati ottenuti, invece con GET non serve action perchè le informazioni ottenute vengono restituite direttamente nell'url della pagina. Per i form è piú utilizzato il metodo POST, in quanto ci vengono date informazioni sensibili. -->
+  <h3>Insersci qui la parola che vuoi censurare dal paragrafo:</h3>
+  <form action="index.php" method="post">
+    <input type="text" name="badword">
+    <input type="submit" value="Cliccami tutto">
+  </form>
+
+  <!-- Form creato con richiesta GET, a differenza di POST ci restituisce "query strings parameters" direttamente nell' url della pagina stessa. Di defaul il Form ha method = Get. -->
+  <!-- <form action="" method="get">
+    <input type="text" name="badword">
+    <input type="submit" value="Cliccami tutto">
+  </form> -->
 
   <!-- VueJS -->
   <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
