3f8bbb8a5ad0eb463aa9067c9a9b5bd23de88f7a
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-28 16:17:07 +0100

    Initial Commit 2.0

---
+++ index.php
@@ -0,0 +1,28 @@
+<!-- PHP -->
+<?php
+
+
+
+?>
+<!-- /PHP -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Badwords</title>
+  <!-- CSS -->
+  <link rel="stylesheet" href="./dist/css/app.css">
+</head>
+
+<body>
+
+
+  <!-- VueJS -->
+  <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+  <!-- JS -->
+  <script src="./dist/js/app.js"></script>
+</body>
+
+</html>
